46fb12031cd909d61c21c03840b15035b81d9913
gario300 <41522264+gario300@users.noreply.github.com>
2019-07-25 20:33:17 -0500

    a

--- config/database.js
+++ config/database.js
@@ -18,7 +18,7 @@ module.exports = {
   | interacting with SQL databases.
   |
   */
-  connection: Env.get('DB_CONNECTION', 'sqlite'),
+  connection: Env.get('DB_CONNECTION', 'mariadb'),
 
   /*
   |--------------------------------------------------------------------------
